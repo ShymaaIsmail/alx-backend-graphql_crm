6c458728f684929bbb3a616ef311102516efdd06
shymaaismail <shymaa.m.ismail@gmail.com>
2025-07-09 19:42:34 +0300

    add __str__ to customer models

--- crm/models.py
+++ crm/models.py
@@ -6,6 +6,9 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
